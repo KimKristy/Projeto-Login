cb4cfdd648b7c21c74abc7a231fab71a9d99bfd7
Kimberly <kkristy1218@gmail.com>
2025-10-09 09:13:46 -0300

    criando a função de logout e o conteúdo do dashboard

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,7 +1,41 @@
-const Dashboard = () => {
-  return <div>
-    <h1>Seja Bem-Vindo ao Dashboard</h1>
-  </div>;
+import { useNavigate } from "react-router-dom";
+import { Chart } from "react-google-charts";
+import data from "../teste.json";
+
+export const options = {
+  isStacked: true,
+  height: 300,
+  legend: { position: "top", maxLines: 3 },
+  vAxis: { minValue: 0 },
 };
 
-export default Dashboard;
+const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h1>Seja Bem-vindo ao Dashboard</h1>
+      <button
+        onClick={handleLogout}
+        className=" mt-6 w-[100px] h-[60px] bg-blue-950 text-white rounded-2xl"
+      >
+        Logout
+      </button>
+
+      <Chart
+        chartType="AreaChart"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+      />
+    </div>
+  );
+};
+
+export default Dashboard
